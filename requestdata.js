3d25084b2e4a71856efdf5b7290dbd4ebd88e11f
justin leggett <justin@codingrookie.com>
2017-06-16 12:27:14 -0500

    added data to requestdata.js

--- requestdata.js
+++ requestdata.js
@@ -1,25 +1,48 @@
 // mongo demoDB ./requestdata.js
 db.posts.save({
-    "Posts" : "Sed ut perspiciatis unde omnis iste natus error",
-    "Company" : "World Outreach Church",
-    "dbid": "faegfnjksbghksb",
-    "Username": "J Smooth",
-    "Likes": 0
+  Posts: "Sed ut perspiciatis unde omnis iste natus error",
+  Company: "World Outreach Church",
+  dbid: "faegfnjksbghksb",
+  Username: "J Smooth",
+  Likes: 0
 });
 
 db.posts.save({
-    "Posts" : "Sed ut perspiciatis unde omnis iste natus error",
-    "Company" : "World Outreach Church",
-    "dbid": "faegfnjwdwdwdksbghksb",
-    "Username": "K Smooth",
-    "Likes": 0
+  Posts: "Sed ut perspiciatis unde omnis iste natus error",
+  Company: "World Outreach Church",
+  dbid: "faegfnjwdwdwdksbghksb",
+  Username: "K Smooth",
+  Likes: 0
 });
 
+db.posts.save({
+  Posts: "Sed ut perspiciatis unde omnis iste natus error",
+  Company: "World Outreach Church",
+  dbid: "faegfnjwdwdwdsdsddksbghksb",
+  Username: "Z Smooth",
+  Likes: 0
+});
 
 db.posts.save({
-    "Posts" : "Sed ut perspiciatis unde omnis iste natus error",
-    "Company" : "World Outreach Church",
-    "dbid": "faegfnjwdwdwdsdsddksbghksb",
-    "Username": "Z Smooth",
-    "Likes": 0
+  Posts: "Sed ut perspiciatis unde omnis iste natus error",
+  Company: "World Outreach Church",
+  dbid: "faegfnjwdwdwdwdwwdsdsddksbghksb",
+  Username: "Justin",
+  Likes: 0
+});
+
+db.posts.save({
+  Posts: "Sed ut perspiciatis unde omnis iste natus error",
+  Company: "World Outreach Church",
+  dbid: "faegfnjwdwddwdwwdwdwwdsdsddksbghksb",
+  Username: "Justin",
+  Likes: 0
+});
+
+db.posts.save({
+  Posts: "Sed ut perspiciatis unde omnis iste natus error",
+  Company: "World Outreach Church",
+  dbid: "faegfnjwdwwqqsaderdwwdsdsddksbghksb",
+  Username: "Justin",
+  Likes: 0
 });
